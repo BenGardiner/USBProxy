3d2dc26a1d85c376be4fe80bacf01457b91e2978
Ben Gardiner <ben.gardiner@irdeto.com>
2015-10-27 17:32:58 -0400

    Revert "Remove mqueu helpers which nothing uses"
    This reverts commit bc7be9825fefbcc56d91fee895d3ba4143ca561e.

---
+++ src/lib/mqueue_helpers.h
@@ -0,0 +1,19 @@
+/*
+ * This file is part of USBProxy.
+ */
+
+#ifndef MQUEUE_HELPERS_H_
+#define MQUEUE_HELPERS_H_
+
+#ifdef __cplusplus
+extern "C"
+{
+#endif
+
+extern int clean_mqueue();
+
+#ifdef __cplusplus
+} // __cplusplus defined.
+#endif
+
+#endif /* MQUEUE_HELPERS_H_ */
